1da9799a40b551e76a3c0f91989fd2c4a8699302
Hopertz <lugano.ngulwa@gmail.com>
2023-12-22 07:33:02 +0300

    add macro definition

--- chapter_two/dweight.c
+++ chapter_two/dweight.c
@@ -1,5 +1,6 @@
 /* Computes the dimensional weight of a 12 11 x 10 11 x a II box  */
 
+#define INCHES_PER_POUND 166
 #include <stdio.h>
 
     int main(void){
@@ -11,7 +12,7 @@
     length = 12;
     width = 10;
     volume = height * length * width;
-    weight = (volume + 165) / 166;
+    weight= (volume +  INCHES_PER_POUND - 1) / INCHES_PER_POUND;
     some_float = 3.24356789;
 
     printf("Dimensions: %dx%dx%d\n", length , width, height);
--- chapter_two/dweight2.c
+++ chapter_two/dweight2.c
@@ -1,5 +1,6 @@
 /* Computes the dimensional weight of a box from input provided by the user */
 
+#define INCHES_PER_POUND 166
 #include <stdio.h>
 
     int
@@ -13,9 +14,8 @@
     scanf("%d", &length);
     printf("Enter width of box: ");
     scanf("%d", &width);
-    width = 10;
     volume = height * length * width;
-    weight = (volume + 165) / 166;
+    weight = (volume + INCHES_PER_POUND - 1) / INCHES_PER_POUND;
     printf("Dimensions: %dx%dx%d\n", length, width, height);
     printf("Volume (cubic inches) : %d\n", volume);
     printf("Dimensional weight (pounds): %d\n", weight);
